8cb37c1da7047a8b09108e1b04b4ec09f929bdc9
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-07-12 19:46:21 +0300

    Add google analitics script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
+import Script from 'next/script';
 import { useRouter } from 'next/router';
 import { CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app';
@@ -11,7 +12,6 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
-
   const router = useRouter();
   const isAdminPanel = useMemo(
     () => router.pathname.startsWith('/admin'),
@@ -27,6 +27,18 @@ export default function App({
 
   return (
     <SessionProvider session={session} refetchInterval={10 * 60}>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+        `}
+      </Script>
       <CssBaseline />
       {isAdminPanel ? (
         <Theme>
