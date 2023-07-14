39fdf08534063aa0c8ab49bdb90aa4c2bf7dea7f
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-07-14 13:42:08 +0300

    Create basic statistic

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head';
 import { General } from '@/utils/constants/general.enum';
-
+import Dashboard from './dashboard';
 export default function IndexAdminPage() {
   return (
     <>
       <Head>
         <title>{`${General.SiteTitle}`}</title>
       </Head>
-      <p style={{ textAlign: 'center' }}>Not avaiable</p>
+      <Dashboard />
     </>
   );
 }
