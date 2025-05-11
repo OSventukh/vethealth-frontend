668aedbcdb5c05badcc7c919fb7e62f4f84749bd
Oleksandr Sventukh <o.sventukh@gmail.com>
2025-05-11 21:16:23 +0000

    fix: replace Image component with Logo component in Header for consistency and improved styling

--- src/app/(public)/components/Header/index.tsx
+++ src/app/(public)/components/Header/index.tsx
@@ -5,6 +5,7 @@ import { api } from '@/api';
 import DesktopNavigation from '../Navigation/Desktop';
 import MobileNavigation from '../Navigation/Mobile';
 import SearchBar from '../Navigation/Search';
+import Logo from '@/components/logo';
 
 export default async function Header({ topic }: { topic?: string }) {
   const categories = await api.categories.getMany({
@@ -18,14 +19,7 @@ export default async function Header({ topic }: { topic?: string }) {
     <header className="max-h-40 bg-[rgb(180,239,232)]">
       <div className="container flex justify-between gap-8 py-8">
         <Link href="/">
-          <Image
-            src="/logo/vethealth-logo.svg"
-            alt="VetHealth"
-            width={300}
-            height={300}
-            priority
-            className="h-auto"
-          />
+          <Logo className='h-16' />
         </Link>
         <div className="flex items-center">
           {isCategories && (
--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
+import { cn } from '@/lib/utils';
 
-export default function Logo() {
+export default function Logo({ className }: { className?: string }) {
   return (
     <Image
       src="/logo/vethealth-logo.svg"
-      width={200}
+      width={300}
       height={200}
       alt="Vethealth"
+      className={cn('h-8 w-auto', className)}
     />
   );
 }
