fcaa717ef5ed6bbce89666f2390d188b4d42e413
Oleksandr Sventukh <o.sventukh@gmail.com>
2025-05-24 20:43:40 +0000

    fix: update SITE_DESCRIPTION for clarity and consistency in messaging

--- src/utils/constants/generals.ts
+++ src/utils/constants/generals.ts
@@ -2,4 +2,4 @@ export const SITE_NAME = 'VetHealth';
 export const SITE_TITLE = `${SITE_NAME} - Лікування та догляд за тваринами`;
 export const NOT_FOUND_TITLE = `Сторінка не знайдена | ${SITE_NAME}`;
 export const SITE_DESCRIPTION =
-  'VetHealth- це платформа для ветеринарних лікарів та власників тварин про різні аспекти лікування та догляду за тваринами.';
+  'Інформація для ветеринарних лікарів та власників тварин про різні аспекти лікування та догляду за тваринами.';
