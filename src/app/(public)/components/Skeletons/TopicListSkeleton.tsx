4037cc4c91eaead7f5a87148bb7480214a8ab7cf
Oleksandr Sventukh <o.sventukh@gmail.com>
2025-06-30 21:49:25 +0000

    fix: add missing key prop to div in TopicListSkeleton component

--- src/app/(public)/components/Skeletons/TopicListSkeleton.tsx
+++ src/app/(public)/components/Skeletons/TopicListSkeleton.tsx
@@ -5,7 +5,7 @@ export default function TopicListSkeleton({ itemsCount = 4 }: Props) {
   return (
     <div className="grid gap-8 md:grid-cols-2">
       {Array.from({ length: itemsCount }).map((_, index) => (
-        <div>
+        <div key={index}>
           <div
             key={index}
             className="h-[240px] w-[240px] rounded-2xl bg-gray-300 transition duration-300 ease-in md:hover:scale-110"
