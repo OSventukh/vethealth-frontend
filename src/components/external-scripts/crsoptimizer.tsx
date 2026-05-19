ef7e0f64bcb4d37321728a6dbc1998e7357cae63
Oleksandr Sventukh <o.sventukh@gmail.com>
2026-05-19 10:34:43 +0300

    fix: comment out unused Script component for clarity

--- src/components/external-scripts/crsoptimizer.tsx
+++ src/components/external-scripts/crsoptimizer.tsx
@@ -4,7 +4,7 @@ export default function CRSOptimizer() {
 	return (
 		<>
 			<Script type="text/javascript" src="https://optimize.vethealth.com.ua/comp/5WjEAtrEuS.js" />
-			<Script src="https://crsoptimizer.conversionrate.store/comp/Eja4hwk8eP.js" />
+			{/* <Script src="https://crsoptimizer.conversionrate.store/comp/Eja4hwk8eP.js" /> */}
 		</>
 		
 	);
