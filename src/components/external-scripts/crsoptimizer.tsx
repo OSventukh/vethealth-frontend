a9cd5957a6b595d27be4af0f4cc7eeb7faa2bc62
Oleksandr Sventukh <o.sventukh@gmail.com>
2026-05-19 10:27:49 +0300

    fix: replace lowercase script tag with Next.js Script component for consistency

--- src/components/external-scripts/crsoptimizer.tsx
+++ src/components/external-scripts/crsoptimizer.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 export default function CRSOptimizer() {
 	return (
 		<>
-			<script type="text/javascript" src="https://optimize.vethealth.com.ua/comp/5WjEAtrEuS.js"></script>
+			<Script type="text/javascript" src="https://optimize.vethealth.com.ua/comp/5WjEAtrEuS.js" />
 			<Script src="https://crsoptimizer.conversionrate.store/comp/Eja4hwk8eP.js" />
 		</>
 		
