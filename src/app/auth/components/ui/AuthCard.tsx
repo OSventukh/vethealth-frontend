7551d7ff6a631c939f179d82047613a01e1722dc
Oleksandr Sventukh <o.sventukh@gmail.com>
2026-06-21 00:09:49 +0300

    fix: adjust layout and loading attribute in AuthCard component

--- src/app/auth/components/ui/AuthCard.tsx
+++ src/app/auth/components/ui/AuthCard.tsx
@@ -9,7 +9,7 @@ export default function AuthCard({ children, title }: Props) {
 	return (
 		<div className="flex h-dvh w-full items-center justify-center bg-blue-50">
 			<div className="bg-background flex h-min w-[90%] overflow-hidden rounded-lg sm:w-[40rem] md:w-[45rem]">
-				<div className="sm-[50%] flex h-full w-full flex-col justify-between p-5">
+				<div className="sm-[50%] flex flex-1 h-full w-full flex-col justify-between p-5">
 					<div className="flex w-full flex-col items-center gap-2">
 						<Image
 							src="/logo/logo.svg"
@@ -17,6 +17,7 @@ export default function AuthCard({ children, title }: Props) {
 							width={500}
 							height={500}
 							className="w-12"
+							loading="eager"
 						/>
 						<h1 className="text-xl font-light uppercase">{title}</h1>
 					</div>
@@ -29,7 +30,7 @@ export default function AuthCard({ children, title }: Props) {
 					width={1000}
 					height={1000}
 					style={{ objectFit: "contain" }}
-					className="hidden w-[50%] sm:block"
+					className="flex-1 hidden w-[50%] sm:block"
 				/>
 			</div>
 		</div>
